7e2f2dd1c90ce1432fbdbe240781560682cdd5fa
jcbritobr <jcbritobr@gmail.com>
2020-09-24 22:31:58 -0300

    Fiz integration tests.

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,22 +1,44 @@
-use std::{sync::Arc, sync::Barrier};
+use std::{sync::Arc, sync::Barrier, sync::atomic::AtomicUsize, sync::atomic::Ordering, sync::mpsc};
 
 use workerpool::pool;
 
 #[test]
-fn should_all_packages_work_together() {
-    let pool = pool::WorkerPool::new(5);
-    let njobs = 5;
-    let nworkers = 7;
+fn pool_should_sum_atomic_variable() {
+    let njobs = 20;
+    let nworkers = 42;
+    let pool = pool::WorkerPool::new(nworkers);
+    let atomic = Arc::new(AtomicUsize::new(0));
     let barrier = Arc::new(Barrier::new(njobs + 1));
 
     assert!(njobs <= nworkers, "too many jobs will deadlock");
 
-    for i in 0..njobs {
+    for _ in 0..njobs {
         let b = barrier.clone();
+        let atomic = atomic.clone();
         pool.execute(Box::new(move || {
-            println!("thread id {}", i);
+            atomic.fetch_add(1, Ordering::Relaxed);
             b.wait();
         }));
     }
     barrier.wait();
+    assert_eq!(atomic.load(Ordering::SeqCst), njobs);
+}
+
+#[test]
+fn pool_should_synchronize_sender_and_receiver_and_fold_results() {
+    let nworkers = 4;
+    let njobs = 8;
+
+    let pool = pool::WorkerPool::new(nworkers);
+
+    let(tx, rx) = mpsc::channel();
+
+    for _ in 0..njobs {
+        let tx = tx.clone();
+        pool.execute(Box::new(move || {
+            tx.send(1).expect("channel waiting for pool");
+        }));
+    }
+
+    assert_eq!(rx.iter().take(njobs).fold(0, |a, b| a+ b), njobs);
 }
